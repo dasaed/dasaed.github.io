e61590c5e09a2175586893ba005337a6eb455c3e
dasaed <carlos.dasaed@gmail.com>
2019-05-17 12:57:17 -0230

    moviing old web tutorials repository here

---
+++ Old_Web_Tutorials/README.md
@@ -0,0 +1,1 @@
+# Old_Web_Tutorials
---
+++ Old_Web_Tutorials/old_index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+ 
+	<head>
+	<meta charset="utf=8"/><!-- This is used to include extra characters not included in a normal(US) keyboard -->
+		<title>Carlos Salcedo  - Semillas del Futuro </title><!--Title is what shows in the tab of the browser-->
+	</head>
+
+<body>
+
+<h1>Semillas de Futuro</h1>
+<h2>Video Motivacional</h2>
+<h2>Por: Carlos Dasaed Salcedo Carreño</h2>
+<p>
+<a href="www.telecoteam.com/inicio">telecoteam.com</a>
+</p>
+<hr>
+
+<p>These tags are new, so not all sources will support these tags(audio or video).<br>
+<br>
+
+
+</p>
+
+<video width="720" height="480" controls autoplay <!--loop-->>
+<source src="semillasdelfuturo_carlossalcedo.mp4" type="video/mp4" />
+
+If you can see this, you need to update your browser to see the video. 
+</video>
+
+
+
+</body>
+
+
+</html>
